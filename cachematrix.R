779a477c32dde95f36cf5bdbd1a6e87f8b97198f
Rachitkh <rachit.kh@gmail.com>
2017-05-16 13:36:24 -0700

    Programming Assignment 2: Lexical Scoping
    This file contains changes for Programming Assignment 2: Lexical Scoping for CourseEra. Please review the same.
    
    Thanks in advance.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below functions help in caching the inverse of the matrix which is sometimes costly computation with huge data sets
 
-## Write a short comment describing this function
+
+## makeCacheMatrix function create a matrix object which can cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL  #intialize inverse
+	
+	#Set value, and set inv=NULL whenever set value
+	
+	set <- function(y) {
+                x <<- y
+                inv <<- NULL
+    }
+	
+	#get value
+	get <- function() {
+				x    #return x
+	}
+	
+    setInverse <- function(inverse){
+				inv <<- inverse  #Set the inverse
+	}
+	
+    getInverse <- function(){
+				inv   #get the inverse
+	}
+	#Return list
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+	
 
 }
 
 
-## Write a short comment describing this function
+## It create inverse of matrix created in makeCacheMatrix. If the inverse already exists, then it pull the data from  cache
+## Thus, helping in saving the computational time
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		inv <- x$getInverse()
+		#Check  if inverse exists in cache
+        if (!is.null(inv)) {
+                message("Cached data Exists. Sending the cache data.")
+                return(inv)
+        }
+		#Cache data doesn't exist
+		message("Cached data doesn't exists.")
+        matr <- x$get()
+        inv <- solve(matr, ...)
+        x$setInverse(inv)
+        inv
+		
 }
